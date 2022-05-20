407b9ba95d296a3c279c2487459dc8be4286d6d1
user <user@DESKTOP-3HBU0VV>
2022-05-20 11:39:26 +0530

    DataTable Changes

--- CucumberProject/src/test/java/com/stepDefinition/LoginSteps.java
+++ CucumberProject/src/test/java/com/stepDefinition/LoginSteps.java
@@ -1,11 +1,13 @@
 package com.stepDefinition;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import io.cucumber.datatable.DataTable;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
@@ -20,19 +22,19 @@ public class LoginSteps {
 		System.out.println("Step1: User is on LoginPage");
 		WebDriverManager.chromedriver().setup();
 		driver = new ChromeDriver();
-	//	driver.manage().window().maximize();
+		// driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
 		driver.get("https://opensource-demo.orangehrmlive.com/index.php/auth/validateCredentials");
 	}
 
-	//@When("user enters username and password")  //normal
-	//@When("^user enters \"(.*)\" and \"(.*)\"$") //regular DataDriven Oproach
+	// @When("user enters username and password") //normal
+	// @When("^user enters \"(.*)\" and \"(.*)\"$") //regular DataDriven Oproach
 	@When("^user enters (.*) and (.*)$")
-	public void user_enters_username_and_password(String uname,String pswd) {
+	public void user_enters_username_and_password(String uname, String pswd) {
 		System.out.println("Step2: User enters username and pswd");
-	//	driver.findElement(By.name("txtUsername")).sendKeys("admin");
-	//	driver.findElement(By.name("txtPassword")).sendKeys("admin123");
+		// driver.findElement(By.name("txtUsername")).sendKeys("admin");
+		// driver.findElement(By.name("txtPassword")).sendKeys("admin123");
 		driver.findElement(By.name("txtUsername")).sendKeys(uname);
 		driver.findElement(By.name("txtPassword")).sendKeys(pswd);
 	}
@@ -50,4 +52,11 @@ public class LoginSteps {
 		driver.close();
 	}
 
+	@When("user enters credentials using DataTable")
+	public void user_enters_credentials_using_DataTable(DataTable dataTable) {
+		System.out.println("Step2: User enters username and pswd");
+		List<List<String>> data = dataTable.cells();
+		driver.findElement(By.name("txtUsername")).sendKeys(data.get(0).get(0));
+		driver.findElement(By.name("txtPassword")).sendKeys(data.get(0).get(1));
+	}
 }
